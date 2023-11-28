1209230365264ead891b125cf0e0dc57a45151e0
TheIdiot01 <73561775+TheIdiot01@users.noreply.github.com>
2023-11-28 01:36:41 -0600

    Add files via upload

---
+++ TF.cmd
@@ -0,0 +1,33 @@
+@echo off
+
+echo 1) NullCore
+echo 2) LMAOBox
+echo 3) LMAOBox Beta
+
+set /p InputVal=What cheat do you want to use: 
+
+"vac bypass location here"
+
+if %InputVal%==1 (
+
+    "nullcore location here"
+
+) else (
+  
+if %InputVal% == 2 (
+
+    "lbox location here"
+
+) else (
+
+    "lbox location here along with -beta"
+
+))
+
+timeout /t 10 /nobreak
+
+start steam://rungameid/440
+
+echo Happy Cheating!
+
+timeout /t 3 /nobreak
